5d9dcaa29a35bd7f4855935f20f776ccce7c54f0
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-20 14:51:44 +0200

    exercise 1 with 2 added tests for each new class

---
+++ tdd-oop-inheritance.CSharp.Test/ItemOfLibraryTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_oop_inheritance.CSharp.Main;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    [TestFixture]
+    public class ItemOfLibraryTests
+    {
+        private ItemOfLibrary item;
+        [SetUp]
+        public void Setup()
+        {
+            item = new ItemOfLibrary("Harry Potter");
+        }
+        [Test]
+        public void NewItemItNotALoanDefualt()
+        {
+            Assert.IsFalse(item.isOnLoan());
+        }
+        [Test]
+        public void NotOnLoanReturnsMessage()
+        {
+            var message = item.checkIn();
+            Assert.AreEqual("item is not currently on loan", message);
+        }
+    }
+}
