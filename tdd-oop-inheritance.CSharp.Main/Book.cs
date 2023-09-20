4e6b9f0978ee393853ce4cd493b836e168852ddd
Ntina B <n_bekiari@outlook.com>
2023-09-20 17:37:26 +0300

    ok .

--- tdd-oop-inheritance.CSharp.Main/Book.cs
+++ tdd-oop-inheritance.CSharp.Main/Book.cs
@@ -1,42 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
-    public class Book {
-        public string title;
+    public class Book : LibraryItem
+    {
+        public Author Author { get; }
 
-        bool onLoan = false;
-
-        public Book(string title) {
-            this.title = title;
-        }
-
-        public bool isOnLoan() {
-            return onLoan;
-        }
-
-        public string checkIn() {
-            if (!this.isOnLoan()) {
-                return "item is not currently on loan";
-            }
-
-            this.onLoan = false;
-
-            return "item has been checked in";
-        }
-
-        public string checkOut() {
-            if (this.isOnLoan()) {
-                return "item is currently on loan";
-            }
-
-            this.onLoan = true;
-
-            return "item has been checked out";
+        public Book(string title, Author author) : base(title)
+        {
+            Author = author;
         }
     }
 }
---
+++ tdd-oop-inheritance.CSharp.Main/LibraryItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;    
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+    public class LibraryItem
+    {
+        public string Title { get; protected set; }
+
+        public bool IsOnLoan { get; protected set; }
+
+        public LibraryItem(string title)
+        {
+            Title = title;
+            IsOnLoan = false;
+        }
+
+        public virtual string CheckIn()
+        {
+            if (!IsOnLoan)
+            {
+                return "Item is not currently on loan!";
+            }
+
+            IsOnLoan = false;
+
+            return "Item has been checked in!";
+        }
+
+        public virtual string CheckOut()
+        {
+            if (IsOnLoan)
+            {
+                return "Item is currently on loan!";
+            }
+
+            IsOnLoan = true;
+
+            return "Item has been checked out!";
+        }
+    }
+}
