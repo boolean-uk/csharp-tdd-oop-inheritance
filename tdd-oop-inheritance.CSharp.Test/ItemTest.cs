4a4b738a69b8b4d7a084f53a90c258f20a6f497d
Victor <victor.adamson@se.experis.com>
2024-01-18 11:11:53 +0100

    Exercise 1

---
+++ tdd-oop-inheritance.CSharp.Test/ItemTest.cs
@@ -0,0 +1,41 @@
+﻿using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class ItemTest
+    {
+        [Test]
+        public void shouldCheckOutIfAvailable()
+        {
+            Item item = new Item("JUnit Rocks");
+            Assert.AreEqual("item has been checked out", item.checkOut());
+        }
+
+        [Test]
+        public void shouldDeclineIfNotAvailableToCheckout()
+        {
+            Item item = new Item("JUnit Rocks");
+            item.checkOut();
+
+            Assert.AreEqual("item is currently on loan", item.checkOut());
+        }
+
+        [Test]
+        public void shouldCheckInIfOnLoan()
+        {
+            Item item = new Item("JUnit Rocks");
+            item.checkOut();
+
+            Assert.AreEqual("item has been checked in",item.checkIn());
+        }
+
+        [Test]
+        public void shouldDeclineCheckInIfNotOnLoan()
+        {
+            Item item = new Item("JUnit Rocks");
+
+            Assert.AreEqual("item is not currently on loan", item.checkIn());
+        }
+    }
+}
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,21 @@
+﻿using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class LibraryTest
+    {
+        Library library;
+        [Test]
+        public void addToStockTest()
+        {
+            library = new Library();
+            Book testBook = new Book("Roadside Picnic");
+            Article testArticle = new Article("The Bronze Age Collapse");
+            Newspaper testNewspaper = new Newspaper("Fake News");
+            Assert.AreEqual(testBook, library.addToStock(testBook).ElementAt(0));
+            Assert.AreEqual(testArticle, library.addToStock(testArticle).ElementAt(1));
+            Assert.AreEqual(testNewspaper, library.addToStock(testNewspaper).ElementAt(2));
+        }
+    }
+}
