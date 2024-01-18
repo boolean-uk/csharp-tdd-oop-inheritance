d21a42b89db13dfdc2435cdac7746423307e339b
Klara Andersson <klara.andersson@se.experis.com>
2024-01-18 10:20:33 +0100

    Tests done

--- tdd-oop-inheritance.CSharp.Main/Newspaper.cs
+++ tdd-oop-inheritance.CSharp.Main/Newspaper.cs
@@ -6,25 +6,17 @@ using System.Threading.Tasks;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
-    public class Newspaper
+    public class Newspaper : Library
     {
-        public string title;
-
-        Boolean onLoan = false;
-
         public Newspaper(string title) {
-            this.title = title;
+            this.name = title;
         }
 
-        public bool isOnLoan() {
-            return onLoan;
-        }
-
-        public string checkIn() {
+        public override string checkIn() {
             return "newspapers are not available for loan";
         }
 
-        public string checkOut() {
+        public override string checkOut() {
             return "newspapers are not available for loan";
         }
     }
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,38 @@
+﻿using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class LibraryTest
+    {
+        [Test]
+        public void CheckItems()
+        {
+            Newspaper newspaper = new Newspaper("The Daily Java");
+            Book book = new Book("Throne of Glass");
+            Article article = new Article("CandyHouse");
+            
+            Library library = new Library();
+            library.items.Add(newspaper);
+            library.items.Add(book);
+            library.items.Add(article);
+
+            Assert.AreEqual(3, library.items.Count());
+        }
+
+        [Test]
+        public void ShouldBeCheckedIn()
+        {
+            Newspaper newspaper = new Newspaper("The Daily Java");
+            Book book = new Book("Throne of Glass");
+            Article article = new Article("CandyHouse");
+
+            Library library = new Library();
+            library.items.Add(newspaper);
+            library.items.Add(book);
+            library.items.Add(article);
+
+            Assert.AreEqual("item has been checked out", library.checkOutItem("Throne of Glass"));
+        }
+    }
+}
