ecfba960dd0222964d1e83145ec3501ff03d6c13
kristianverduin <kristianverduin@gmail.com>
2024-01-19 10:29:09 +0100

    Finished exercises and tests

---
+++ tdd-oop-inheritance.CSharp.Main/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+    public class Author
+    {
+        private string _name;
+        private string _info;
+        private string _url;
+        public Author(string name, string contactInfo, string website) 
+        {
+            _name = name;
+            _info = contactInfo;
+            _url = website;
+        }
+        public string Name { get => _name; }
+        public string ContactInfo { get => _info; }
+        public string Website { get => _url; }
+    }
+}
---
+++ tdd-oop-inheritance.CSharp.Test/AuthorTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using tdd_oop_inheritance.CSharp.Main;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class AuthorTest
+    {
+        [Test]
+        public void shouldHaveProperties()
+        {
+            Author author = new Author("Kristian", "Contact info", "Website");
+
+            Assert.That(author.Name, Is.EqualTo("Kristian"));
+            Assert.That(author.ContactInfo, Is.EqualTo("Contact info"));
+            Assert.That(author.Website, Is.EqualTo("Website"));
+        }
+    }
+}
