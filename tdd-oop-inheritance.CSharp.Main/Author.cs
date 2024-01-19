d651e793ab7fdc42a4c96f016bb462dee8a7e4a2
Kanthee Kosolyuthasarn <Kanthee>
2024-01-19 11:04:19 +0100

    Kanthee Kosolyuthasarn - C#3 - 19.01.2024

---
+++ tdd-oop-inheritance.CSharp.Main/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+   public class Author
+{   
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Website { get; set; } 
+
+}
+}
--- tdd-oop-inheritance.CSharp.Main/Library.cs
+++ tdd-oop-inheritance.CSharp.Main/Library.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Objects;
+using tdd_oop_inheritance.CSharp.Main.Objects;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
     public class Library {
-        List<Article> articles = new List<Article>();
+        /*List<Article> articles = new List<Article>();
         List<Book> books = new List<Book>();
-        List<Newspaper> newspapers = new List<Newspaper>();
+        List<Newspaper> newspapers = new List<Newspaper>();*/
+        public List<Publication> _publications = new List<Publication>();  
 
-        public void addToStock(Article item) {
-            this.articles.Add(item);
+        public void addToStock(Publication item) {
+            this._publications.Add(item);
         }
 
-        public void addToStock(Book item) {
+        /*public void addToStock(Book item) {
             this.books.Add(item);
         }
 
         public void addToStock(Newspaper item) {
             this.newspapers.Add(item);
-        }
+        }*/
 
         // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
         // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
-        public string checkInArticle(string title) {
+        public string checkIn(string title) {
 
-            List<Article> filtered = (List<Article>)this.articles.Where(article => article.title.Equals(title));
+            List<Publication> filtered = (List<Publication>)this._publications.Where(obj => obj.Title.Equals(title)).ToList();
 
             if (filtered.Count() < 1) {
                 return "item is not part of the library's collection";
@@ -36,8 +39,8 @@ namespace tdd_oop_inheritance.CSharp.Main
             return filtered[0].checkIn();
         }
 
-        public string checkOutArticle(string title) {
-            List<Article> filtered = (List<Article>)this.articles.Where(article => article.title.Equals(title));
+        public string checkOut(string title) {
+            List<Publication> filtered = (List<Publication>)this._publications.Where(obj => obj.Title.Equals(title)).ToList();
 
             if (filtered.Count() < 1) {
                 return "item is not part of the library's collection";
@@ -46,7 +49,7 @@ namespace tdd_oop_inheritance.CSharp.Main
             return filtered[0].checkOut();
         }
 
-        public string checkInBook(string title) {
+        /*public string checkInBook(string title) {
             List<Book> filtered = (List<Book>)this.books.Where(book => book.Equals(title));
 
             if (filtered.Count() < 1) {
@@ -84,6 +87,6 @@ namespace tdd_oop_inheritance.CSharp.Main
             }
 
             return filtered[0].checkOut();
-        }
+        }*/
     }
 }
