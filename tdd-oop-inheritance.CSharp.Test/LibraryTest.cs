9e1ca57f42a4477be6c00f2a4058518975627fa7
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-01-19 10:25:18 +0100

    Refactored the code and added tests to see if the library works, which it passes now

---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_oop_inheritance.CSharp.Main;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    internal class LibraryTest
+    {
+        private Library library;
+        [SetUp] 
+        public void SetUp() 
+        {
+            library = new Library();
+            library.addToStock(new Article("JUnit Rocks"));
+            library.addToStock(new Book("Hitchiker's guide to the galaxy"));
+            library.addToStock(new Newspaper("New York Times"));
+        }
+
+        [Test]
+        public void shouldCheckOutIfAvailable()
+        {
+            Assert.That(library.checkOut("JUnit Rocks"), Is.EqualTo("item has been checked out"));
+        }
+
+
+        [Test]
+        public void shouldDeclineIfNotAvailableToCheckout()
+        {
+            library.checkOut("JUnit Rocks");
+
+            Assert.That(library.checkOut("JUnit Rocks"), Is.EqualTo("item is currently on loan"));
+        }
+
+        [Test]
+        public void shouldCheckInIfOnLoan()
+        {
+            library.checkOut("JUnit Rocks");
+            Assert.That(library.checkIn("JUnit Rocks"), Is.EqualTo("item has been checked in"));
+        }
+
+        [Test]
+        public void shouldDeclineCheckInIfNotOnLoan()
+        {
+        }
+
+        [Test]
+        public void shouldBeUnavailableForCheckIn()
+        {
+            Assert.That(library.checkIn("New York Times"), Is.EqualTo("newspapers are not available for loan"));
+        }
+
+        [Test]
+        public void shouldBeUnavailableForCheckOut()
+        {
+            Assert.That(library.checkOut("New York Times"), Is.EqualTo("newspapers are not available for loan"));
+        }
+    }
+}
