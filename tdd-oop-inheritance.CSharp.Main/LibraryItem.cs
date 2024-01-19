dd6e5b07d106d1988357dfc9293713f8570ae326
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-19 09:13:46 +0100

    Refactor library item related classes

---
+++ tdd-oop-inheritance.CSharp.Main/LibraryItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+    public abstract class LibraryItem
+    {
+        public string title;
+
+        bool onLoan = false;
+
+        public LibraryItem(string title)
+        {
+            this.title = title;
+        }
+
+        public bool isOnLoan()
+        {
+            return onLoan;
+        }
+
+        virtual public string checkIn()
+        {
+            if (!this.isOnLoan())
+            {
+                return "item is not currently on loan";
+            }
+
+            this.onLoan = false;
+
+            return "item has been checked in";
+        }
+
+        virtual public string checkOut()
+        {
+            if (this.isOnLoan())
+            {
+                return "item is currently on loan";
+            }
+
+            this.onLoan = true;
+
+            return "item has been checked out";
+        }
+    }
+}
