f22fa11b0db304ea7fec5d41bc56559351b51f09
Caroline Berg <caroline.berg@no.experis.com>
2024-01-19 19:37:42 +0100

    Inheritance and dependency

--- tdd-oop-inheritance.CSharp.Main/Newspaper.cs
+++ tdd-oop-inheritance.CSharp.Main/Newspaper.cs
@@ -1,31 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
-    public class Newspaper
+    public class Newspaper : InventoryItem,Item
     {
-        public string title;
-
-        Boolean onLoan = false;
-
+       
         public Newspaper(string title) {
-            this.title = title;
+            this.Title = title;
         }
 
-        public bool isOnLoan() {
-            return onLoan;
-        }
-
-        public string checkIn() {
+        public new string checkIn()
+        { return "newspapers are not available for loan"; }
+       
+        public new string checkOut() {
             return "newspapers are not available for loan";
         }
 
-        public string checkOut() {
-            return "newspapers are not available for loan";
-        }
+      
     }
 }
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,68 @@
+﻿using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    class LibraryTest
+    {
+        [Test]
+        public void shouldCheckOutIfAvailable()
+            {   
+                Article article = new Article("JUnit Rocks");
+            Library library = new Library();
+            library.addToStock(article);
+           Assert.That(article.isOnLoan() == false, Is.True);
+           Assert.AreEqual("item has been checked out", library.checkOut(article));
+            }
+
+        [Test]
+        public void shouldDeclineIfNotAvailableToCheckout()
+            {
+            Library lib = new Library();
+            Article article = new Article("JUnit Rocks");
+            lib.addToStock(article);
+            lib.checkOut(article);
+
+            Assert.AreEqual("item is currently on loan", lib.checkOut(article));
+        }
+
+        [Test]
+        public void shouldCheckInIfOnLoan()
+            {
+            Library lib = new Library();
+            Book book = new Book("C#Unit 3 Rocks");
+            lib.addToStock(book);
+            lib.checkOut(book); 
+                
+                Assert.AreEqual("item has been checked in", lib.checkIn(book));
+            }
+
+        [Test]
+        public void shouldDeclineifnotInList()
+            {
+            Author author = new Author(name: "Jane Johnson", adress: "jane@johnson.com", website: "");
+                Article article = new Article("JUnit Rocks", author);
+                Library lib = new Library();
+
+                Assert.AreEqual("item is not part of the library's collection", lib.checkIn(article));
+            }
+        [Test]
+        public void checkifAuthorIsSet()
+        {
+            Author author = new Author(name: "Jane Johnson", adress: "jane@johnson.com", website: "");
+            Article article = new Article("JUnit Rocks", author);  
+
+            Assert.AreEqual("Jane Johnson",article.Author.Name );
+        }
+        [Test]
+        public void checkifAuthorIsSetEmpty()
+        {
+            Book book = new Book("JUnit Rocks");
+            Library lib = new Library();
+            lib.addToStock(book);
+
+            Assert.IsTrue(book._author.Name == string.Empty);
+        }
+
+    }
+}
