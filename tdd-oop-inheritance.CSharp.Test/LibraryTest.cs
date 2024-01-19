0ac43c6e3bce643dbe546f5bd72bdf04eb5782b6
Matthias Greeven <matsgreeven@gmail.com>
2024-01-19 10:11:06 +0100

    Added LibraryTest

---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,44 @@
+﻿using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class LibraryTest
+    {
+        Library library = new Library();
+        [SetUp]
+        public void SetUp()
+        {
+            Book checkedOutbook = new Book("checkOut");
+            checkedOutbook.checkOut();
+            library.paperThings.Add(checkedOutbook);
+            library.paperThings.Add(new Book("testBook"));
+            library.paperThings.Add(new Book("testArticle"));
+            library.paperThings.Add(new Book("testPaper"));
+
+        }
+        [Test]
+        public void shouldAddToStock()
+        {
+            Book book = new Book("JUnit Rocks");
+            library.addToStock(book);
+            Assert.IsTrue(library.paperThings.Contains(book));
+        }
+        [Test]
+        public void shouldCheckIn()
+        {
+            Assert.AreEqual("item has been checked in", library.checkInPaperThing("checkOut"));
+        }
+        [Test]
+        public void shouldCheckOut()
+        {
+            Assert.AreEqual("item has been checked out", library.checkOutPaperThing("testBook"));
+        }
+        [Test]
+        public void shouldNotBeInLibrary()
+        {
+            Assert.AreEqual("item is not part of the library's collection", library.checkInPaperThing("wrongBook"));
+        }
+
+    }
+}
