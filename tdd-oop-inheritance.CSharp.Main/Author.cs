8bb5e3ec0c84c15e20ba4cf6cc3999f6cb162f68
Aleksander Kolsrud <kols203@gmail.com>
2024-01-24 10:18:43 +0100

    Finished task

---
+++ tdd-oop-inheritance.CSharp.Main/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Website { get; set; }
+
+        public Author(string name, string contact, string website)
+        {
+            Name = name;
+            Contact = contact;
+            Website = website;
+        }
+    }
+}
--- tdd-oop-inheritance.CSharp.Test/ArticleTest.cs
+++ tdd-oop-inheritance.CSharp.Test/ArticleTest.cs
@@ -8,14 +8,16 @@ namespace tdd_oop_inheritance.CSharp.Test
         [Test]
         public void shouldCheckOutIfAvailable()
             {
-                Article article = new Article("JUnit Rocks");
+            Author author = new Author("Jane Doe", "jane.doe@example.com", "https://janedoe.com");
+            Article article = new Article("JUnit Rocks", author);
                 Assert.AreEqual("item has been checked out", article.checkOut());
             }
 
         [Test]
         public void shouldDeclineIfNotAvailableToCheckout()
             {
-                Article article = new Article("JUnit Rocks");
+            Author author = new Author("Jane Doe", "jane.doe@example.com", "https://janedoe.com");
+            Article article = new Article("JUnit Rocks", author);
                 article.checkOut();
 
                 Assert.AreEqual("item is currently on loan", article.checkOut());
@@ -24,7 +26,8 @@ namespace tdd_oop_inheritance.CSharp.Test
         [Test]
         public void shouldCheckInIfOnLoan()
             {
-                Article article = new Article("JUnit Rocks");
+            Author author = new Author("Jane Doe", "jane.doe@example.com", "https://janedoe.com");
+            Article article = new Article("JUnit Rocks", author);
                 article.checkOut();
 
                 Assert.AreEqual("item has been checked in", article.checkIn());
@@ -33,9 +36,22 @@ namespace tdd_oop_inheritance.CSharp.Test
         [Test]
         public void shouldDeclineCheckInIfNotOnLoan()
             {
-                Article article = new Article("JUnit Rocks");
+            Author author = new Author("Jane Doe", "jane.doe@example.com", "https://janedoe.com");
+            Article article = new Article("JUnit Rocks", author);
 
                 Assert.AreEqual("item is not currently on loan", article.checkIn());
             }
+
+        [Test]
+
+        public void shouldSetAuthorName()
+        {
+            Author author = new Author("Jane Doe", "jane.doe@example.com", "https://janedoe.com");
+            Article article = new Article("JUnit Rocks", author);
+
+            string name = article.Author.Name;
+
+            Assert.AreEqual("Jane Doe", name);
+        }
     }
 }
