e934e50fee0f6487fafaf9c28c44b09de3f96dc1
Rose van der Giessen <Rose van der Giessen>
2024-02-07 11:42:04 +0100

    Exercise 1 complete

---
+++ tdd-oop-inheritance.CSharp.Main/LibraryItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance
+{
+    public class LibraryItem
+    {
+        public string title;
+
+        bool onLoan = false;
+        public LibraryItem(string title) 
+        {
+            this.title = title;
+        }
+
+        public bool isOnLoan()
+        {
+            return onLoan;
+        }
+
+        public virtual string checkIn()
+        {
+            if (!this.isOnLoan())
+            {
+                return "item is not currently on loan";
+            }
+            this.onLoan = false;
+            return "item has been checked in";
+        }
+
+        public virtual string checkOut()
+        {
+            if (this.isOnLoan())
+            {
+                return "item is currently on loan";
+            }
+            this.onLoan = true;
+            return "item has been checked out";
+        }
+    }
+}
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryItemTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using tdd_oop_inheritance.CSharp.Main;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class LibraryItemTest
+    {
+        [Test]
+        public void shouldCheckOutIfAvailable()
+        {
+            LibraryItem item = new LibraryItem("JUnit Rocks");
+            Assert.AreEqual("item has been checked out", item.checkOut());
+        }
+
+        [Test]
+        public void shouldDeclineIfNotAvailableToCheckout()
+        {
+            LibraryItem item = new LibraryItem("JUnit Rocks");
+            item.checkOut();
+
+            Assert.AreEqual("item is currently on loan", item.checkOut());
+        }
+
+        [Test]
+        public void shouldCheckInIfOnLoan()
+        {
+            LibraryItem item = new LibraryItem("JUnit Rocks");
+            item.checkOut();
+
+            Assert.AreEqual("item has been checked in", item.checkIn());
+        }
+
+        [Test]
+        public void shouldDeclineCheckInIfNotOnLoan()
+        {
+            LibraryItem item = new LibraryItem("JUnit Rocks");
+
+            Assert.AreEqual("item is not currently on loan", item.checkIn());
+        }
+    }
+}
+
