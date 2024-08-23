bb8fbebe019a0c1c96ac7f5c2fc973681e06a945
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-23 11:44:04 +0200

    Moved everything from Newspaper, Book and Article to a new class named LibraryItem

---
+++ tdd-oop-inheritance.CSharp.Main/LibraryItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_inheritance.CSharp.Main
+{
+    public abstract class LibraryItem
+    {
+        private string _title;
+        private bool _onLoan;
+
+        public string Title { get => _title; set => _title = value; }
+        public bool OnLoan { get => _onLoan; set => _onLoan = value; }
+
+        public LibraryItem(string title) 
+        {
+            _title = title;
+        }
+
+        public virtual bool isOnLoan()
+        {
+            return _onLoan;
+        }
+
+        public virtual string checkIn()
+        {
+            if (!isOnLoan())
+            {
+                return "item is not currently on loan";
+            }
+
+            _onLoan = false;
+
+            return "item has been checked in";
+        }
+
+        public virtual string checkOut()
+        {
+            if (isOnLoan())
+            {
+                return "item is currently on loan";
+            }
+
+            _onLoan = true;
+
+            return "item has been checked out";
+        }
+    }
+}
