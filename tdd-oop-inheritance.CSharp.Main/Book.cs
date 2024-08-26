c067b2083753b0df4fdb1eaac01dae9392d76f99
George <george.alexander.saav@gmail.com>
2024-08-26 09:18:02 +0200

    Exercise 1

--- tdd-oop-inheritance.CSharp.Main/Book.cs
+++ tdd-oop-inheritance.CSharp.Main/Book.cs
@@ -6,37 +6,10 @@ using System.Threading.Tasks;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
-    public class Book {
-        public string title;
+    public class Book : Item {
 
-        bool onLoan = false;
-
-        public Book(string title) {
+        public Book(string title) : base (title) {
             this.title = title;
-        }
-
-        public bool isOnLoan() {
-            return onLoan;
-        }
-
-        public string checkIn() {
-            if (!this.isOnLoan()) {
-                return "item is not currently on loan";
-            }
-
-            this.onLoan = false;
-
-            return "item has been checked in";
-        }
-
-        public string checkOut() {
-            if (this.isOnLoan()) {
-                return "item is currently on loan";
-            }
-
-            this.onLoan = true;
-
-            return "item has been checked out";
         }
     }
 }
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using tdd_oop_inheritance.CSharp.Main;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    class LibraryTest
+    {
+        
+        [Test]
+        public void ShouldBookAddToStock()
+        {
+            Library library = new Library();
+            Book book = new Book("C# is the thing");
+
+            library.addToStock(book);
+            bool expected = true;
+
+            bool result = false;
+
+            if (library.items.Contains(book)) 
+            {
+                result = true;
+            }
+
+            Assert.IsTrue(result == expected);
+            
+        }
+
+        [Test]
+        public void ShouldAddArticleToStock()
+        {
+            Library library = new Library();
+            Article article = new Article("This article is a non issue");
+
+            library.addToStock(article);
+            bool expected = true;
+
+            bool result = false;
+
+            if (library.items.Contains(article))
+            {
+                result = true;
+            }
+
+            Assert.IsTrue(result == expected);
+
+        }
+
+        [Test]
+        public void ShouldAddToStock()
+        {
+            Library library = new Library();
+            Newspaper paper = new Newspaper("The Daily Journal");
+
+            library.addToStock(paper);
+            bool expected = true;
+
+            bool result = false;
+
+            if (library.items.Contains(paper))
+            {
+                result = true;
+            }
+
+            Assert.IsTrue(result == expected);
+
+        }
+    }
+}
