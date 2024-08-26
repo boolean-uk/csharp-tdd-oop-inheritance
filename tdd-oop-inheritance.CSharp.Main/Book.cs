42df9f0b3408cc14490575fb1561a35bc240c98e
Kaja Thoresen Plaszko <kaja.plaszko@no.experis.com>
2024-08-26 09:53:37 +0200

    Kaja Plaszko

--- tdd-oop-inheritance.CSharp.Main/Book.cs
+++ tdd-oop-inheritance.CSharp.Main/Book.cs
@@ -6,37 +6,14 @@ using System.Threading.Tasks;
 
 namespace tdd_oop_inheritance.CSharp.Main
 {
-    public class Book {
+    public class Book : LibraryItem {
         public string title;
 
         bool onLoan = false;
 
-        public Book(string title) {
-            this.title = title;
+        public Book(string title) : base(title)
+        {
         }
 
-        public bool isOnLoan() {
-            return onLoan;
-        }
-
-        public string checkIn() {
-            if (!this.isOnLoan()) {
-                return "item is not currently on loan";
-            }
-
-            this.onLoan = false;
-
-            return "item has been checked in";
-        }
-
-        public string checkOut() {
-            if (this.isOnLoan()) {
-                return "item is currently on loan";
-            }
-
-            this.onLoan = true;
-
-            return "item has been checked out";
-        }
     }
 }
---
+++ tdd-oop-inheritance.CSharp.Test/LibraryTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    internal class LibraryTest
+{
+}
+}
